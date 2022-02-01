2ad0c05e05435db3cf978d02993f5a51f5c5cd9f
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-01 15:39:59 -0800

    bug: Cannot get the js to respond to inputs. Nothing showing up in console regarding inputted numbers or in UI.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,31 @@ const robotLanguage = ['Beep', 'Boop', 'Will you be my neighbor?']
 
 
 var inputAction = function(input) {
-  let number = [];
+  let numbers = [];
  for (let i = 0; i <= input; i++); {
-   number.push(i.toString());
+   numbers.push(i.toString());
 };
 
-if (number.includes("3")) {
-  return number = robotLanguage[2];
-}else if (number.includes("2")) {
-  return number = robotLanguage[1];
-}else if (number.includes("1")) {
-  return number = robotLanguage[0]
-} else {
-  return number;
-};
-};
+const inputActions = numbers(function(number) {
+  if (number.includes("3")) {
+    return number = robotLanguage[2];
+  }else if (number.includes("2")) {
+    return number = robotLanguage[1];
+  }else if (number.includes("1")) {
+    return number = robotLanguage[0]
+  } else {
+    return number;
+  };
+  });
+  return inputActions;
+  };
+
+$(document).ready(funtion() {
+  $("form#number").submit(function(event) {
+    const inputAction = parseInt($("input#input").val());
+    $("inputAction").text(result);
+    $("resut").show();
+    event.preventDefault();
+  })
+})
+

cd2fcd03a484dd274aa6eb4398f8e78f6f13a22a
michaelshamrell <michaelshamrell2001@gmail.com>
2022-03-02 18:13:11 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,38 @@
-$(document).ready(function() {
-  $("form#number").submit(function(event) {
-    const numberInput = parseInt($("number#input").val());
-    const result = numberInput(input);
-    $("#result").text(number);
-    $("#result").show();
-    event.preventDefault();
-  });
-});
+"use strict";
 const robotLanguage = ['Beep', 'Boop', 'Will you be my neighbor?']
-
+//FOR EACH LOOP
 const numberInput = function(input) {
-  let number = [];
+  let numbers = [];
  for (let i = 0; i <= input; i++); {
-   number.push(i.toString());
+   numbers.push(i.toString());
+   console.log(input)
+   console.log(numbers)
 };
-
-const numberInput = number.map(function(number) {
-  if (number.includes("3")) {
-    return number = robotLanguage[2];
-  }else if (number.includes("2")) {
-    return number = robotLanguage[1];
-  }else if (number.includes("1")) {
-    return number = robotLanguage[0];
+//IF STATEMENT FOR RETURN
+const robotLanguages = number.map(function(number) {
+  if (numbers.includes("3")) {
+    return numbers = robotLanguage[2];
+  }else if (numbers.includes("2")) {
+    return numbers = robotLanguage[1];
+  }else if (numbers.includes("1")) {
+    return numbers = robotLanguage[0];
   } else {
-    return number;
+    return numbers;
       };
     });
-    return numberInput;
+    return robotLanguages;
   };
+  $(document).ready(function() {
+    $("form#number").submit(function(event) {
+      const input = parseInt($("number#input").val());
+      // const result = numberInput(input);
+      // $("#result").text(number);
+      // $("#result").show();
+      event.preventDefault();
+      console.log(numberInput)
+      console.log(input)
+  });
+});
 
 
 
---
+++ js/scripts1.0.js
@@ -0,0 +1,19 @@
+const robotLanguage = ['Beep', 'Boop', 'Will you be my neighbor?']
+
+function numberInput (inputArray) {
+  var numbers = []
+  for (let i = 0; i <= inputArray; i += 1) {
+    var roboReturn = i.toString();
+
+    if (robotLanguage.includes(3))
+      return numbers = robotLanguage[]
+
+
+
+
+  }
+}
+
+
+
+
